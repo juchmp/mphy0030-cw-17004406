1a690bda52ef367abf0561cca48c00e6722fa475
Juliette Champaud <juliette_champaud@LAPTOP-D87CQPD4.localdomain>
2021-01-11 10:02:35 +0000

    Commit 2 - draft task 3

---
+++ part1/task3/defs.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def gradient_descent(f, var, coeffs, step, n_iter, tol, grad_func):
+
+    ''' The function outputs the optimal function variable value  
+ that minimises the multivariate function (output) value. '''
+    count = 0
+    rate = 0.01
+   
+    while min(step) > tol and count < n_iter:
+        pre_vars = var
+        var = var - rate*grad_func(f, pre_vars, coeffs)
+        step = abs(var - pre_vars)
+        count = count + 1
+        #print(var, count, step, min(step))
+    #for i in range(1, n_iter):
+     #   step = 2*step
+      #  while f(vars - step*grad) >= f(vars):
+       #     step = 1/2
+        #vars = vars - step*grad
+
+    return var
+
+def finite_difference_gradient(f, var, coeffs):
+
+    '''The function outputs all the estimated partial  
+    derivatives for the input vector variable.'''
+    grad = []
+    h = 1e-5
+    for i, _ in enumerate(var):
+        d = []
+        for j, vars_j in enumerate(var):
+            if i==j:
+                d.append(vars_j + h)
+            else:
+                d.append(vars_j)
+        diff = (f(d, coeffs) - f(var, coeffs)) / h
+        grad.append(diff)
+
+    return np.array(grad)
+
+def second_derivative(gradf, f, var, coeffs):
+    ''' Function outputting the first derivative of the given quadratic polynomial '''
+
+    h = 1e-5
+    grad_at_opt = gradf(f, var, coeffs)
+    grad_at_h = gradf(f, var+h, coeffs) 
+    diff = (grad_at_h - grad_at_opt)/h
+
+    return diff
---
+++ part1/task3/task.py
@@ -0,0 +1,58 @@
+import numpy as np
+from numpy.polynomial import polynomial
+from defs import finite_difference_gradient, gradient_descent, second_derivative
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def quadratic_polynomial(x, a):
+
+    ''' Computes a multivariate polynomial of degree 2 for three 
+    variables. Returns a scalar value.'''     
+    
+    y = a[1]*x[0]**2 + a[2]*x[1]**2 + a[3]*x[2]**2 + a[4]*x[0]*x[1] + \
+        a[5]*x[0]*x[2] + a[6]*x[1]*x[2] + a[7]*x[0] + a[8]*x[1] + a[9]*x[2] + a[0]
+
+    return y
+
+x_init = np.array([2,2,2]).reshape((3,1)) # reasonable guess for y to be min
+
+a = np.random.rand(10,1)*10
+y = quadratic_polynomial(x_init, a)
+
+''' Finite difference'''
+grad = finite_difference_gradient(quadratic_polynomial, x_init, a)
+
+'''Gradient descent '''
+opt_x = gradient_descent(quadratic_polynomial, x_init, a, np.array([1,1,1]), 1000, 0.00001, finite_difference_gradient)
+print(opt_x)
+min_y = quadratic_polynomial(opt_x, a)
+print(min_y)
+
+''' Arithmetic verification of gradient descent result''' 
+# Check gradient at points outputted by gradient descent
+grad_min = finite_difference_gradient(quadratic_polynomial, opt_x, a)
+print(grad_min) # outputs close to 0
+
+# Second derivative check
+grad2_min = second_derivative(finite_difference_gradient, quadratic_polynomial, opt_x, a)
+print(grad2_min)
+
+''' Visual verification of gradient descent result '''
+
+fig = plt.figure()
+x1 = np.linspace(opt_x[0]-10, opt_x[0]+10)
+x2 = np.linspace(opt_x[1]-10, opt_x[1]+10)
+x1, x2 = np.meshgrid(x1, x2)
+x3 = [opt_x[2]-10, opt_x[2], opt_x[2]+10]
+
+ax = fig.add_subplot(1, 3, 1, projection='3d') 
+ax.contourf(x1, x2, quadratic_polynomial([x1,x2,x3[0]],a))
+
+ax = fig.add_subplot(1,3,2, projection='3d')
+ax.contourf(x1, x2, quadratic_polynomial([x1,x2,x3[1]],a))
+ax.scatter(opt_x[0], opt_x[1], min_y, c='r')
+
+ax = fig.add_subplot(1,3,3, projection='3d')
+ax.contourf(x1, x2, quadratic_polynomial([x1,x2,x3[2]],a))
+
+plt.show()
