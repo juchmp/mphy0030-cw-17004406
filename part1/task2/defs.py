3c941c93157d34b97cc5b38b888c64b304043f6d
Juliette Champaud <juliette_champaud@LAPTOP-D87CQPD4.localdomain>
2021-02-02 22:08:40 +0000

    Commit 14 - task 2 improvement

--- part1/task2/defs.py
+++ part1/task2/defs.py
@@ -3,21 +3,52 @@ from numpy.linalg import det, multi_dot, inv
 from numpy import pi, exp, matmul, sqrt, var, log, abs
 from scipy.stats import norm
 
-def tri_gaussian_pdf(x, mu, sig):
+def gaussian_pdf(x, mu, sig):
+
+    ''' The function outputs the probability density for each 
+    set of vector x.
+    
+    Parameters
+    ----------
+    x : array-like, shape(number of samples, number of variables)
+        Samples of vector x.
+    mu : array-like, shape(number of variables,)
+        A vector of the samples' means for each variable.
+    sig : array-like, shape(number of variables, number of variables)
+        Covariance matrix.
+        
+    Returns
+    -------
+    pdf : array-like, shape(number of samples,)
+        Probability densities for each sample of vector x.'''
 
     N = ((2*pi)**(3/2))*sqrt(det(sig))
-    mat_mul = np.einsum('...j,jk,...k', x-mu, inv(sig), x-mu)
+    a = np.matmul(x-mu, inv(sig))
+    mat_mul = np.einsum('jk, kj -> j', a, (x-mu).T)
     pdf = exp(-1/2 * mat_mul) / N
-    #for i in range(x.shape[0]):
-     #   p = 1 / (2*pi*sqrt(det(sig))) \
-      #      * exp(-1/2*multi_dot([np.atleast_2d(x[i,:]-mu), inv(sig), np.atleast_2d(x[i,:]-mu).T]).astype('float128'))
-       # pdf.append(p)
-    #pdf = np.array(pdf)
 
     return pdf
 
 def solve_eq(x, perc):
 
+    ''' The function outputs the roots solution to a quadratic equation
+    which represents the univariate probability density function when 
+    equal to each percentile. 
+    Rearranging we get: ax^2 + bx + c = 0 with a, b, c described below.
+    
+    Parameters
+    ----------
+    x : array-like, shape(number of samples,)
+        Function variables.
+    perc : scalar
+        Percentile to be evaluated.
+    
+    Returns 
+    -------
+    r1, r2 : scalars 
+        Roots solving quadratic equation.
+    
+    '''
     a = 1
     b = -2*np.mean(x)
     c = np.mean(x)**2 + 2*var(x)*log(perc*sqrt(var(x)*2*pi)) 
@@ -34,9 +65,26 @@ def solve_eq(x, perc):
 
 def circle_cords(roots):
     
-    width = abs(roots[0]-roots[1])
-    height = abs(roots[2]-roots[3])
-    xcord = (width)/2 + roots[1]
-    ycord = (height)/2 + roots[3]
+    ''' Function outputs the parameters needed to draw circles for each percentile.
+    
+    Parameters
+    ----------
+    roots : array-like, shape(number of variables, 2)
+        Vector containing the roots in each direction.
+    
+    Returns
+    -------
+    xcord : scalar
+        X coordinate of the circle's center.
+    ycord : scalar
+        Y coordinate of the circle's center.
+    width : scalar
+        Radius of the circle.
+    
+    '''
+    width = abs(roots[0][0]-roots[0][1])
+    height = abs(roots[1][0]-roots[1][1])
+    xcord = (width)/2 + roots[0][1]
+    ycord = (height)/2 + roots[1][1]
 
     return xcord, ycord, width
--- part1/task2/task.py
+++ part1/task2/task.py
@@ -1,51 +1,54 @@
 import numpy as np
 from numpy import random as rd
-from defs import tri_gaussian_pdf, solve_eq, circle_cords
+from defs import gaussian_pdf, solve_eq, circle_cords
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, PathPatch
 from mpl_toolkits.mplot3d import Axes3D, art3d
 
-x = rd.rand(10000, 3) # randomly generating 10,000 samples of vector x
+# Randomly generating 10,000 samples of vector x
+x = rd.rand(10000, 3) 
+
+# Means and covariance matrices
+mu = np.mean(x, axis=0)
+sig = np.cov(x, rowvar=False)
+
+# Probability densities and percentiles
+pdf = gaussian_pdf(x, mu, sig)
+perc10, perc50, perc90 = np.percentile(pdf, [10,50,90])
+
+# First step in plotting percentiles 
+# Finding the coordinates of the probability densities equal to each percentile.
+roots10 = []
+roots50 = []
+roots90 = []
+for i in range(x.shape[1]):
+    roots10.append(solve_eq(x[:,i], perc10))
+    roots50.append(solve_eq(x[:,i], perc50))
+    roots90.append(solve_eq(x[:,i], perc90))
+
+''' Plotting of the surfaces representing the respective percentiles '''
+
 x1 = np.sort(x[:,0], axis=None)
 x2 = np.sort(x[:,1], axis=None)
 x1, x2 = np.meshgrid(x1, x2)
-
-mu = np.mean(x, axis=0)
-sig = np.cov(x)
-
-#pos = np.empty(x1.shape + (2,), dtype='float32') # creates empty array of 10000 by 10000 by 2
-#pos[:,:,0] = x1 # set first 10000 by 10000 grid to x1 values and the next to x2 values
-#pos[:,:,1] = x2
-
-pdf = tri_gaussian_pdf(x, mu, sig)
-print(pdf)
-perc10, perc50, perc90 = np.percentile(pdf, [10,50,90])
-
-
-r1x1_10, r2x1_10 = solve_eq(x1, perc10)
-r1x2_10, r2x2_10 = solve_eq(x2, perc10)
-roots10 = np.array([r1x1_10, r2x1_10, r1x2_10, r2x2_10])
-
-r1x1_50, r2x1_50 = solve_eq(x1, perc50)
-r1x2_50, r2x2_50 = solve_eq(x2, perc50)
-roots50 = np.array([r1x1_50, r2x1_50, r1x2_50, r2x2_50])
-
-r1x1_90, r2x1_90 = solve_eq(x1, perc90)
-r1x2_90, r2x2_90 = solve_eq(x2, perc90)
-roots90 = np.array([r1x1_90, r2x1_90, r1x2_90, r2x2_90])
-
-# Plots
+pdf[::-1].sort()
+pdf = np.meshgrid(pdf, pdf)
 
 fig = plt.figure()
 ax = fig.gca(projection = '3d')
-ax.plot_surface(x1, x2, pdf, linewidth=1)
 
-xcord10, ycord10, width10 = circle_cords(roots10)
-c10 = Circle((xcord10,ycord10), width10/2, fill=0, color='r')
-xcord50, ycord50, width50 = circle_cords(roots50)
-c50 = Circle((xcord50,ycord50), width50/2, fill=0, color='y')
+# Surface of cdf to visualise the probability densities
+plot = ax.plot_surface(x1, x2, pdf[0], linewidth=1, label='CDF equivalent of computed pdf')
+plot._edgecolors2d = plot._edgecolors3d
+plot._facecolors2d = plot._facecolors3d
+
+# Circle plots of respective percentiles
 xcord90, ycord90, width90 = circle_cords(roots90)
-c90 = Circle((xcord90,ycord90), width90/2, fill=0, color='g')
+c90 = Circle((xcord90,ycord90), width90/2, alpha=0.8, color='g', label='90th percentile')
+xcord50, ycord50, width50 = circle_cords(roots50)
+c50 = Circle((xcord50,ycord50), width50/2, alpha=0.8, color='r', label='50th percentile')
+xcord10, ycord10, width10 = circle_cords(roots10)
+c10 = Circle((xcord10,ycord10), width10/2, alpha=0.8, color='y', label='10th percentile')
 
 ax.add_patch(c10)
 ax.add_patch(c50)
@@ -54,10 +57,7 @@ art3d.patch_2d_to_3d(c10, z=perc10, zdir='z')
 art3d.patch_2d_to_3d(c50, z=perc50, zdir='z')
 art3d.patch_2d_to_3d(c90, z=perc90, zdir='z')
 
-#ax.plot_surface(x1[pdf==perc90], x2[pdf==perc90], cont90)
-#ax.scatter(x1, x2, cont90, zdir='z')
-#ax.set_zlim(0,2)
-#ax.set_zticks(np.linspace(0,1,5))
-#ax.view_init(27, -21)
-plt.show()
+ax.legend()
 
+#plt.show()
+plt.savefig('task2.png')
