b2b1c227c8b1797aea3fbf9052b9b6d8ddfc29ee
Juliette Champaud <juliette_champaud@LAPTOP-D87CQPD4.localdomain>
2021-02-03 10:47:03 +0000

    Commit 15 - task 1 improvement

--- part1/task1/defs.py
+++ part1/task1/defs.py
@@ -1,9 +1,20 @@
 import numpy as np
-from array import array
 
 def simple_image_write(image):
 
-    ''' Function that writes given image file into a binary file '''
+    ''' 
+    Function that writes given image file into a binary file.
+    
+    Parameters
+    ----------
+    image : array-like, shape(image.shape)
+        Matrix of voxel/pixel values representing given image.
+        
+    Returns
+    -------
+    outfile : binary file
+        Image values stored in binary format.
+    '''
 
     outfile = '../../data/image.sim'
     image.tofile(outfile)
@@ -12,9 +23,21 @@ def simple_image_write(image):
 
 def simple_image_read(outfile):
 
-    ''' Function that reads the previously written binary file so it can be viewed '''
+    ''' 
+    Function that reads the previously written binary file so it can be viewed.
+    
+    Parameters
+    ----------
+    outfile : binary file
+        Contains image values in binary format.
+        
+    Returns
+    -------
+    output : image file
+        Matrix of pixel/voxel values representing the image previously stored in outfile.
+    
+    '''
 
-    #with open('../../data/image.sim', 'r') as bfile:
     output = np.fromfile(outfile, dtype='int16')
 
     return output
--- part1/task1/task.py
+++ part1/task1/task.py
@@ -3,19 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from defs import simple_image_write, simple_image_read
 
+
+# Image loading
 mat_data = loadmat('../../data/example_image.mat')
 vol = np.array(mat_data['vol'], dtype='int16')
-#print(vol[:50])
 voxdims = mat_data['voxdims'].astype('float32')
 
+# Image writing
 file_to_read = simple_image_write(vol)
 
-vol_bin = simple_image_read(file_to_read) # currently a 1d array but want 3d array
+# Image reading
+vol_bin = simple_image_read(file_to_read) 
 vol_bin = np.reshape(vol_bin, vol.shape)
 
-## Plots
+''' Plotting '''
 
-# First choice of z-coordinate
-slice1 = vol_bin[:,:,20]
-plt.imshow(slice1,cmap='gray')
-plt.show()
+z = [10, 15, 20]
+fig, ax = plt.subplots(1, 3)
+for i, j in enumerate(z):
+    ax[i%3].imshow(vol_bin[:,:,j],cmap='gray')
+    ax[i%3].set_title('Slice z = %d' %j)
+fig.tight_layout(pad=2.0)
+#plt.show()
+plt.savefig('task1.png')
